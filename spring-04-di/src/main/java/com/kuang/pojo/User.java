92eb47cf9d8b0ad59eaadfa32bf68fb242d478e5
chenp <641760512@qq.com>
2022-09-06 11:30:14 +0800

    依赖注入DI-扩展：p命名空间注入 properties

---
+++ spring-04-di/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,30 @@
+package com.kuang.pojo;
+
+public class User {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
