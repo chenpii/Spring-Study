ab71b852e56d5316fca1358ee5c42d75fcaf11ce
chenp <641760512@qq.com>
2022-09-06 14:04:05 +0800

    依赖注入DI-c命名空间 构造器注入

--- spring-04-di/src/main/java/com/kuang/pojo/User.java
+++ spring-04-di/src/main/java/com/kuang/pojo/User.java
@@ -4,6 +4,14 @@ public class User {
     private String name;
     private int age;
 
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public int getAge() {
         return age;
     }
--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.kuang.pojo.Student;
+import com.kuang.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -22,4 +24,19 @@ public class MyTest {
          */
 
     }
+
+    @Test
+    public void test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans2.xml");
+        User user = context.getBean("user2", User.class);
+        System.out.println(user);
+    }
+
 }
