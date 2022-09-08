ea6eddc68b2c9d78fb3edd0288e1441b717f3326
chenp <641760512@qq.com>
2022-09-08 17:34:57 +0800

    spring实现aop

---
+++ spring-09-aop/src/main/java/com/kuang/log/AfterLog.java
@@ -0,0 +1,6 @@
+package com.kuang.log;
+
+import org.springframework.aop.AfterAdvice;
+
+public class AfterLog implements AfterAdvice {
+}
--- spring-09-aop/src/main/java/com/kuang/log/Log.java
+++ spring-09-aop/src/main/java/com/kuang/log/Log.java
@@ -13,6 +13,6 @@ public class Log implements MethodBeforeAdvice {
      * o:目标对象
      */
     public void before(Method method, Object[] objects, Object o) throws Throwable {
-
+        System.out.println(o.getClass().getName() + "的" + method.getName() + "方法被执行了");
     }
 }
